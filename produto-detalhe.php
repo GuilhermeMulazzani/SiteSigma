8afb674b821d7820dc93764cf3d6187702beb03f
GuilhermeMulazzani <guimamu2006@gmail.com>
2022-06-23 11:48:08 -0300

    Update produto-detalhe.php

--- produto-detalhe.php
+++ produto-detalhe.php
@@ -6,6 +6,14 @@ include_once './includes/_header.php';
 ?>
 
 <h1>Produto Detalhe</h1>
+<div class="card" style="width: 18rem;">
+        <img src="<?php echo $produtos[$i]['imagem'];?>" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
+            <p class="card-text"><?php echo $produtos[$i]['descricao'];?></p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
 
 <?php
 // include do footer
