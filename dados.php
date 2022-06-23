73f7233521e1c73b6ad22efa9add2c2e4832fe47
GuilhermeMulazzani <guimamu2006@gmail.com>
2022-06-23 11:16:52 -0300

    Atualization of the project

---
+++ dados.php
@@ -0,0 +1,13 @@
+<?php
+$produtos = array(
+    0 => array('nome' => 'Guilherme','Mula'=>'Sem preço bb','descricao'=>'Guigui pika das galaxias','imagem'=>'./content/capivara.jpg'),
+    1 => array('nome' => 'Capivarinha assassina','precinho'=>'666','descricao'=>'revoltada','imagem'=>'./content/capivara.jpg'),
+    2 => array('nome' => 'Guigui banana','preco'=>'1000','descricao'=>'show de bola bolinha bolao','imagem'=>'./content/capivara.jpg'),
+    3 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    4 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    5 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    6 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    7 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>''),
+    8 => array('nome' => '','preco'=>'','descricao'=>'','imagem'=>'')
+);
+?>
