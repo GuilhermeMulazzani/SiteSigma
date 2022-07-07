5c0373922775dab9abc4f4bfc7779d3483e70f41
GuilhermeMulazzani <guimamu2006@gmail.com>
2022-07-07 10:45:55 -0300

    atualization in the consulta

--- produto-detalhe.php
+++ produto-detalhe.php
@@ -1,19 +1,37 @@
+
+
 <?php
 // include do footer
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
 ?>
-
+<div class"container">
 <h1>Produto Detalhe</h1>
-<div class="card" style="width: 18rem;">
-        <img src="<?php echo $produtos[$i]['imagem'];?>" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
-            <p class="card-text"><?php echo $produtos[$i]['descricao'];?></p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
+</div>
+
+<?php
+$id= $_GET [ 'id' ];
+$tipo=$_GET['tipo'];
+?>
+<div id="detalhe" class="container">
+    <div id="produto-detalhe">
+<h3><?php echo $produtos[$id]['nome'] ?></h1>
+<img  src="./content/<?php echo $produtos[$id]['imagem']   ?>" alt="Imagem de capa do card">
+<div class="card-body">
+    
+    <p class="card-text"><?php echo $produtos[$id]['descricao'];?></p>
+    <h4>R$ <?php  echo $produtos[$id]['preco']; ?></h4>
+    <div class="row">
+        <div class="col"><button class="btn btn-danger"><a href="index.php">voltar</a></button> </div>
+        <div class="col"> <button class="btn btn-success"><a href="#">comprar</a></button> </div>
+        
+    <div>
+    
+  </div>
+  </div>
+  </div> 
+
 
 <?php
 // include do footer
